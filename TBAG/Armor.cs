8e2542a241dd47b6c8a1222958830712da28c4c1
Nick Sharrett <NicholasSharrett@letu.edu>
2019-03-22 17:02:30 -0500

    added skelton structure for gameobject classes

---
+++ TBAG/Armor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Armor: Equippable
+{ 
+    private double DEFENSE;
+
+	public Armor()
+	{
+	}
+}
--- TBAG/TBAG/Form1.Designer.cs
+++ TBAG/TBAG/Form1.Designer.cs
@@ -38,9 +38,10 @@
             // 
             // newGameButton
             // 
-            this.newGameButton.Location = new System.Drawing.Point(200, 200);
+            this.newGameButton.Location = new System.Drawing.Point(300, 308);
+            this.newGameButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.newGameButton.Name = "newGameButton";
-            this.newGameButton.Size = new System.Drawing.Size(75, 23);
+            this.newGameButton.Size = new System.Drawing.Size(112, 35);
             this.newGameButton.TabIndex = 0;
             this.newGameButton.Text = "New Game";
             this.newGameButton.UseVisualStyleBackColor = true;
@@ -48,9 +49,10 @@
             // 
             // loadGameButton
             // 
-            this.loadGameButton.Location = new System.Drawing.Point(200, 251);
+            this.loadGameButton.Location = new System.Drawing.Point(300, 386);
+            this.loadGameButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.loadGameButton.Name = "loadGameButton";
-            this.loadGameButton.Size = new System.Drawing.Size(75, 23);
+            this.loadGameButton.Size = new System.Drawing.Size(112, 35);
             this.loadGameButton.TabIndex = 1;
             this.loadGameButton.Text = "Load Game";
             this.loadGameButton.UseVisualStyleBackColor = true;
@@ -58,9 +60,10 @@
             // 
             // settingsButton
             // 
-            this.settingsButton.Location = new System.Drawing.Point(200, 301);
+            this.settingsButton.Location = new System.Drawing.Point(300, 463);
+            this.settingsButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.settingsButton.Name = "settingsButton";
-            this.settingsButton.Size = new System.Drawing.Size(75, 23);
+            this.settingsButton.Size = new System.Drawing.Size(112, 35);
             this.settingsButton.TabIndex = 2;
             this.settingsButton.Text = "Settings";
             this.settingsButton.UseVisualStyleBackColor = true;
@@ -68,9 +71,10 @@
             // 
             // helpButton
             // 
-            this.helpButton.Location = new System.Drawing.Point(200, 356);
+            this.helpButton.Location = new System.Drawing.Point(300, 548);
+            this.helpButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.helpButton.Name = "helpButton";
-            this.helpButton.Size = new System.Drawing.Size(75, 23);
+            this.helpButton.Size = new System.Drawing.Size(112, 35);
             this.helpButton.TabIndex = 3;
             this.helpButton.Text = "Help";
             this.helpButton.UseVisualStyleBackColor = true;
@@ -78,9 +82,10 @@
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(200, 414);
+            this.exitButton.Location = new System.Drawing.Point(300, 637);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.Size = new System.Drawing.Size(112, 35);
             this.exitButton.TabIndex = 4;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = true;
@@ -89,25 +94,27 @@
             // titleLabel
             // 
             this.titleLabel.AutoSize = true;
-            this.titleLabel.Font = new System.Drawing.Font("Edwardian Script ITC", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.titleLabel.Location = new System.Drawing.Point(73, 76);
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(110, 117);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(333, 94);
+            this.titleLabel.Size = new System.Drawing.Size(735, 138);
             this.titleLabel.TabIndex = 5;
             this.titleLabel.Text = "The Quest for the Random\r\nUndecided Shiny Object";
             this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // MainMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.ClientSize = new System.Drawing.Size(893, 867);
             this.Controls.Add(this.titleLabel);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.helpButton);
             this.Controls.Add(this.settingsButton);
             this.Controls.Add(this.loadGameButton);
             this.Controls.Add(this.newGameButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainMenu";
             this.Text = "The Quest for the Random Undecided Shiny Object";
             this.ResumeLayout(false);
