622adb2d54485232bede299967f2886680044fb3
Nick Sharrett <nicholas_sharrett@bellsouth.net>
2019-03-29 17:51:15 -0500

    Create Room.cs

---
+++ TBAG/Classes/Room.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Room : GameObject
+{
+    private Boolean PLAYERLOCATION;
+    private Room NORTH;
+    private Room SOUTH;
+    private Room EAST;
+    private Room WEST;
+    private String[] CONTENTS;
+
+    public Room()
+	{
+	}
+
+    //initializes room with references to all adjacent rooms
+    public Room(Room north, Room south, Room east, Room west)
+    {
+        NORTH = north;
+        SOUTH = south;
+        EAST = east;
+        WEST = west;
+
+    }
+}
