9180e5d06b4350f3186bba535bb01af77f8fc831
LHelbig <lisahelbig@letu.edu>
2019-03-29 18:20:32 -0500

    VERY basic game screen functionality added

--- TBAG/TBAG/Form2.cs
+++ TBAG/TBAG/Form2.cs
@@ -12,9 +12,14 @@ namespace TBAG
 {
     public partial class GameScreen : Form
     {
+        static string[] textBoxText = new string[500];
+        static int currentIndex = 0;
         public GameScreen()
         {
             InitializeComponent();
+            textBoxText[currentIndex] = "Please enter some text:";
+            storyText.Text = string.Join(" ", textBoxText);
+            currentIndex++;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -29,9 +34,11 @@ namespace TBAG
         {
             storyText.SelectionLength = storyText.Text.Length + 1;
             string input = inputBox.Text;
-            storyText.Text.Insert(storyText.SelectionLength, "\n > " + input);
-            
+            textBoxText[currentIndex] = "\r\n > " + input;
+            currentIndex++;
+            storyText.Text = string.Join(" ", textBoxText);
             inputBox.ResetText();
         }
+
     }
 }
