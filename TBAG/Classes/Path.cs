df1c3fbfd18bc5f86c975137e337c37ce71dfbae
LHelbig <lisahelbig@letu.edu>
2019-04-01 12:27:11 -0500

    added a path class and adventure class

---
+++ TBAG/Classes/Path.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Path
+{
+    private Boolean NORTH, SOUTH, EAST, WEST;
+    private Boolean CURRENT;
+
+    public Path()
+    {
+
+    }
+
+    public Path(Boolean north, Boolean south, Boolean east, Boolean west, Boolean current)
+    {
+        NORTH = north;
+        SOUTH = south;
+        EAST = east;
+        WEST = west;
+        CURRENT = current;
+    }
+
+    public updateCurrent(Boolean current)
+    {
+        CURRENT = current;
+    }
+}
